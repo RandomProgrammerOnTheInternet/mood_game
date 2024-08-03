eb98e2473f10b0e0bbb7ef35a2b0c61c2292f622
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-08-03 15:58:55 -0400

    setting the mood
    haha nice pun

--- vert.glsl
+++ vert.glsl
@@ -1,18 +1,16 @@
 #version 410 core
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTexCoord;
+out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
 
-out vec3 ourColor;
-out vec2 TexCoord;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform sampler2D ourTexture;
 
 void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    ourColor = aColor;
-    TexCoord = aTexCoord;
+    const float lg2 = 1.442695;
+    float z = gl_FragCoord.z / gl_FragCoord.w;
+    z = z / 5.0;
+    float factor = 1.0 - clamp(exp2(z * z * lg2) - 1.0, 0.0, 1.0);
+    FragColor = mix(vec4(vec3(0.0), 1.0), texture(ourTexture, TexCoord), factor);
 }
