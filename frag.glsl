e66f100e1911f4ebd3ac58ea04ccccad57ac2e88
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-09-24 18:44:41 -0400

    somehow the fog got broken

--- frag.glsl
+++ frag.glsl
@@ -10,7 +10,7 @@ uniform sampler2D ourTexture;
 void main() {
     const float lg2 = 1.442695;
     float z = gl_FragCoord.z / gl_FragCoord.w;
-    z = z / 10.0;
+    z = z / 2.0;
     float factor = 1.0 - clamp(exp2(z * z * lg2) - 1.0, 0.0, 1.0);
-    FragColor = mix(vec4(vec3(0.85), 1.0), texture(ourTexture, TexCoord), factor);
+    FragColor = mix(vec4(vec3(0.0), 1.0), texture(ourTexture, TexCoord), factor);
 }
