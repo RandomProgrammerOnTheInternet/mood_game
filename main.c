dc30d9e951f92e21056a720bb6892f0f4b8a6684
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-11-26 15:09:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -134,7 +134,7 @@ int main() {
 					GL_NEAREST_MIPMAP_NEAREST);
 	gl_tex_parameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	gl_tex_image2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB,
+	gl_tex_image2d(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB,
 				 GL_UNSIGNED_BYTE, data);
 	gl_generate_mipmap(GL_TEXTURE_2D);
 
